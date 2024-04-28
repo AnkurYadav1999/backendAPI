9f542b81196019bca92b323a65db8d8c9da8ce09
Ankur Yadav <ankur.june1999@gmail.com>
2024-04-28 15:21:34 +0530

    installed express and created a server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Welcome to ebook API")
+import app from './src/app.js'
+
+const startServer = () => {
+    const port = process.env.PORT || 3000
+
+    app.listen(port,()=>{
+        console.log(`Server is running at port: ${3000}`)
+    })
+}
+
+startServer()
